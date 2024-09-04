01ea5704cd34395c576e8b754232716a62a8669c
Shwetank <143577113+Shwetank-nitp@users.noreply.github.com>
2024-09-04 12:21:16 +0530

    Update multer.middleware.js

--- src/middleweres/multer.middleware.js
+++ src/middleweres/multer.middleware.js
@@ -1,13 +1,20 @@
 import multer from "multer";
+import path from "path";
+
+const __dirname = path.dirname("");
+
+// Convert the relative path to an absolute path
+const relativePath = "src/assets";
+const absolutePath = path.resolve(__dirname, relativePath);
 
 const storageCustom = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "./src/assets");
+    cb(null, absolutePath); // Save files to the absolute path
   },
   filename: (req, file, cb) => {
-    const filename = Date.now() + "_" + file.originalname;
-    req.filepath = "./src/assets/" + filename;
-    console.log(req.filepath);
+    const filename = Date.now() + "_" + file.originalname.replace(/\s+/g, "");
+    req.filepath = path.join(absolutePath, filename); // Use path.join for correct file path
+    console.log(req.filepath); // Log the filepath for debugging
     cb(null, filename);
   },
 });
